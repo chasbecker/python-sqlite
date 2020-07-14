555884981c09dd9bd72bb7ca3808489ccbf40e19
Charles Becker <c.l.becker@outlook.com>
2020-07-14 10:26:08 -0700

    commit 2
    i still have no idea

---
+++ hw.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 14 10:23:24 2020
+
+@author: Charles
+"""
+
+
+urname = input("So, what's you're name? ")
+print( "Nice to meet you, " + urname + ". See you later")
